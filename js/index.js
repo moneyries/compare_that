630af4d6318a72271a005ae17a41a34358d457e1
Brian Ries <bkries@gmail.com>
2015-07-07 21:08:52 -0400

    Establish 'compare that'

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(function() {
+//starter code
+
+	$('#submit').click(compare);
+
+	function compare () {
+		
+		var a = $('#a').val();
+		console.log(a);
+
+		var b = $('#b').val();
+		console.log(b);
+	}
+
+
+
+	//click event
+
+	//track 2 inputs and comparison value
+
+	//compare the two input values
+
+	//conditional 
+
+	//if a < b
+
+	//if a > b
+
+	//if a == b
+
+	//manipululate 'comparison' html w/ either >, > or ==
+
+});
